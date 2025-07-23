017d3e0afe116c8ebe5aee11150d000cc3b7641d
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-07-23 18:04:46 +0200

    Fuck it

--- src/zoos/Zoos.tsx
+++ src/zoos/Zoos.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Zoos.css';
 import { Zoo } from '../models/zoo';
 import { apiCall } from '../api/apiCall';
+import ZooIdStore from '../api/storeZooId';
 
 function Zoos() {
+  const navigate = useNavigate();
   const [zoos, setZoos] = useState<Zoo[]>([]);
   const [isLoading,setIsLoading] = useState<boolean>(true)
 
@@ -21,6 +24,11 @@ function Zoos() {
     fetchZoos();
   }, []);
 
+  function handleClick(zooId : string){
+    ZooIdStore.setZooId(zooId)
+    navigate('/espaces');
+  }
+
   if (isLoading) {
     return <p>Loading zoos...</p>;
   }
@@ -31,8 +39,13 @@ function Zoos() {
         <p>No zoos available.</p>
       ) : (
         zoos.map((zoo) => (
-          <div key={zoo._id} className="zoo-card">
-            {zoo.name}
+          <div key={zoo._id} className="zoo-card" onClick={() => handleClick(zoo._id)}>
+            <h3>{zoo.name}</h3>
+            <p>Created: {new Date(zoo.createdAt).toLocaleDateString(undefined, {
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric'
+            })}</p>
           </div>
         ))
       )}
